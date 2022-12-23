96f07433580af74e9b558fd789dcfff3191e602b
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 20:10:03 -0300

    feat: :zap: turn app into spa application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import { Home } from './pages/Home/Home.jsx';
+
+import { GlobalStyle } from './assets/globalStyles/globalStyle.js';
+
 function App() {
 
   return (
+    <>
+      <GlobalStyle />
 
-    <h1>Vite + React</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
+
 
   )
 }
 
-export default App
+export default App;
