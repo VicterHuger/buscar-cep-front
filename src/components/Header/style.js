12c5538cd5c2828bfd9a9935d5bb962d2335444e
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 20:01:55 -0300

    feat: :lipstick: add a style to header of the application

---
+++ src/components/Header/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  position:fixed;
+  top:0;
+  left:0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  width: 100%;
+  background-color: rgb(0, 65, 134);
+
+  img{
+    height: 4rem;
+    width: auto;
+  }
+`;
