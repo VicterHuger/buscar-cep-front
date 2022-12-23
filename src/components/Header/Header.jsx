e76d4844fd8669580a4be86853f7a81782ab348a
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 20:03:00 -0300

    feat: :sparkles: add a header component to the application

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style.js"
+
+export function Header() {
+
+  return (
+    <Container>
+      <img src="https://wp.rededorsaoluiz.com.br/wp-content/uploads/2022/01/RedeDor-SaoLuiz-2.svg" alt="Rede D'Or is written in ilustration" />
+    </Container>
+  );
+
+}
